e291d2555c5ae9ecfc4b959110bf3a966ef1195d
Shivmani Singh <anshs9333@gmail.com>
2025-01-30 15:24:50 +0530

    Added updated java script code to day1quest1.js

---
+++ java script streak/day1quest1.js
@@ -0,0 +1,12 @@
+// this is just a demo one to see git push
+// // what will be outout see notes for understading the concep dry run
+let greet = ' HELLO';
+function changeGreet(){
+    let greet = "namaste";
+    console.log(greet);
+    function innerGreen(){
+        console.log(greet);
+    }
+}
+console.log(greet);
+changeGreet();
